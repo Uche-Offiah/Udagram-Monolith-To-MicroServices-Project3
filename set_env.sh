b421e4b4d7633a110f505f96a0d151378f2fe2c2
Uche Offiah <uoffiah@bluetag-it.com>
2022-07-16 19:14:34 +0100

    Refactored Udagram monolith into microservices

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=UchechukwuOffiah
+export POSTGRES_PASSWORD=UchechukwuOffiah35
+export POSTGRES_HOST=uchechukwuoffiah-dev.c6ypozmldpt5.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=uche-1975146-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=UchechukwuOffiah
+export JWT_SECRET=testing
+export URL=http://localhost:8100
